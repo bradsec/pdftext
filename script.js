b59a15c4f9a8f200a2f583151838e7fd493aef26
BRADSEC <mtbradley@gmail.com>
2023-04-26 14:01:20 +1000

    Update CSS

--- script.js
+++ script.js
@@ -117,4 +117,44 @@ copyButton.addEventListener('click', async () => {
     }
   });
   
+  document.addEventListener('DOMContentLoaded', () => {
+    const themeSwitcher = document.getElementById('theme-switcher');
+    const footerCopyrightEl = document.getElementById('footer-copyright');
+
+    function setTheme(theme) {
+        const body = document.body;
+        const themeColorMeta = document.querySelector('meta[name="theme-color"]');
+    
+        if (theme === 'dark') {
+            body.setAttribute('data-theme', 'dark');
+            themeSwitcher.innerText = "LIGHT-THEME";
+            themeColorMeta.content = "#202020";
+        } else {
+            body.removeAttribute('data-theme');
+            themeSwitcher.innerText = "DARK-THEME";
+            themeColorMeta.content = "#FFFFFF";
+        }
+    }
   
+    // Load saved theme from local storage
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      setTheme(savedTheme);
+    }
+  
+    themeSwitcher.addEventListener('click', () => {
+      const body = document.body;
+      const currentTheme = body.getAttribute('data-theme');
+      const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+      setTheme(newTheme);
+      localStorage.setItem('theme', newTheme);
+    });
+
+    // Get the author meta tag content and current year
+    const authorMetaTag = document.querySelector('meta[name="author"]');
+    const authorName = authorMetaTag ? authorMetaTag.content : 'Your Name';
+    const currentYear = new Date().getFullYear();
+
+    // Set the footer content
+    footerCopyrightEl.innerHTML = `${authorName} ${currentYear}`;
+}); 
